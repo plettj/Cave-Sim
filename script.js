7ab6e47e2a8079c8999f3d370fcc409fa84ebcf9
plettj <jlplett303@gmail.com>
2020-08-31 11:53:18 -0700

    Canvas drawing works nicely now.

--- script.js
+++ script.js
@@ -6,23 +6,41 @@ document.body.style.setProperty("--unit", unit + "px");
 document.body.style.setProperty("--pixel", unit / 12 + "px");
 
 var B = document.getElementById("BackgroundCanvas");
-B.width = unit * 32;
-B.height = unit * 32;
+B.width = unit * 10;
+B.height = unit * 10;
 var bctx = B.getContext('2d');
 
 //ctx.drawImage(img, sx, sy, swidth, sheight, x, y, width, height)
 
-var walkingTileset = new Image();
-walkingTileset.src = "WalkingAnimationFirstTileset.png";
-
-function Character(x, y, img) {
-  this.x = x;
-  this.y = y;
-  this.img = img;
+function clear(context, x = 0, y = 0, width = unit * 10, height = unit * 10) {
+  context.clearRect(x, y, width, height);
 }
-Character.draw = function (sx, sy) {
-  bctx.drawImage(this.img, sx, sy, 12, 12, this.x, this.y, unit, unit);
+
+var walkingTileset = new Image();
+walkingTileset.src = "WalkingAnimationFirstTileset800.png";
+
+class Character {
+  constructor(x, y, img) {
+    this.x = x;
+    this.y = y;
+    this.img = img;
+  }
+  draw(sx, sy) {
+    bctx.drawImage(this.img, sx, sy, 96, 96, this.x, this.y, unit * 1, unit * 1);
+  }
 }
 
 var char1 = new Character(unit * 3, unit * 3, walkingTileset);
-char1.draw(0, 0);
+var x = 0
+function loopDraw() {
+  if (x === 48 * 8) x = 0;
+  clear(bctx);
+  char1.draw(x, 0);
+  x += 12 * 8;
+  setTimeout(loopDraw, 125);
+}
+
+
+walkingTileset.onload = function () {
+  loopDraw();
+}
